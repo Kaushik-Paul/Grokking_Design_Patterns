8c42c98a16df0b096201ffcd7623138777f865fe
kaushik <kaushik.paul755@gmail.com>
2024-05-05 14:38:21 +0530

    Add SpiralMatrix Updated

--- src/com/example/matrix/SpiralMatrix.java
+++ src/com/example/matrix/SpiralMatrix.java
@@ -36,6 +36,32 @@ public class SpiralMatrix {
         return resultMatrix;
     }
 
+    // O(n) time and O(n) space
+    public static List<Integer> spiralOrder2(int[][] array) {
+        List<Integer> integerList = new ArrayList<>();
+        int startRow = 0, endRow = array.length - 1;
+        int startCol = 0, endCol = array[0].length - 1;
+        while (startRow <= endRow && startCol <= endCol) {
+            for (int col = startCol; col <= endCol; col++) {
+                integerList.add(array[startRow][col]);
+            }
+            for (int row = startRow + 1; row <= endRow; row++) {
+                integerList.add(array[row][endCol]);
+            }
+            for (int col = endCol - 1; col >= startCol; col--) {
+                integerList.add(array[endRow][col]);
+            }
+            for (int row = endRow - 1; row >= startRow + 1; row--) {
+                integerList.add(array[row][startCol]);
+            }
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
+        }
+        return integerList;
+    }
+
     public static void main(String[] args) {
         int[][][] inputs = {
                 {{1}},
@@ -49,7 +75,7 @@ public class SpiralMatrix {
             System.out.println((i + 1) + ".\tMatrix:");
             Arrays.stream(inputs[i]).map(Arrays::toString).forEach(System.out::println);
 
-            System.out.println("\n\tSpiral order: " + spiralOrder(inputs[i]));
+            System.out.println("\n\tSpiral order: " + spiralOrder2(inputs[i]));
 
             System.out.println();
             System.out.println(new String(new char[100]).replace('\0', '-'));
