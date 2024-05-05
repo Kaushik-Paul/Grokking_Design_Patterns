793b612603e0c976a649b9cdda7d1811d8b125fb
kaushik <kaushik.paul755@gmail.com>
2024-05-05 13:26:57 +0530

    Add SpiralMatrix

---
+++ src/com/example/matrix/SpiralMatrix.java
@@ -0,0 +1,59 @@
+package com.example.matrix;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SpiralMatrix {
+
+    // O(n ^ 2) time and O(n) space
+     private static List<Integer> spiralOrder(int[][] input) {
+        List<Integer> resultMatrix = new ArrayList<>();
+        int rowLength = input.length;
+        int colLength = input[0].length;
+        int row = 0;
+        int col = -1;
+        int direction = 1;
+
+        while (rowLength > 0 && colLength > 0) {
+            for (int i = 0; i < colLength; i++) {
+                col += direction;
+                resultMatrix.add(input[row][col]);
+            }
+            rowLength--;
+
+            for (int i = 0; i < rowLength; i++) {
+                row += direction;
+                resultMatrix.add(input[row][col]);
+            }
+
+            colLength--;
+
+            direction *= -1;
+
+        }
+
+        return resultMatrix;
+    }
+
+    public static void main(String[] args) {
+        int[][][] inputs = {
+                {{1}},
+                {{6}, {2}},
+                {{2, 14, 8}, {12, 7, 14}},
+                {{3, 1, 1}, {15, 12, 13}, {4, 14, 12}, {10, 5, 11}},
+                {{10, 1, 14, 11, 14}, {13, 4, 8, 2, 13}, {10, 19, 1, 6, 8}, {20, 10, 8, 2, 12}, {15, 6, 8, 8, 18}}
+        };
+
+        for (int i = 0; i < inputs.length; i++) {
+            System.out.println((i + 1) + ".\tMatrix:");
+            Arrays.stream(inputs[i]).map(Arrays::toString).forEach(System.out::println);
+
+            System.out.println("\n\tSpiral order: " + spiralOrder(inputs[i]));
+
+            System.out.println();
+            System.out.println(new String(new char[100]).replace('\0', '-'));
+        }
+    }
+
+}
